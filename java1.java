81fc43e5b85c6468681b6a1a41f9cb28f6c3b13f
wuningfei520 <43206761+wuningfei520@users.noreply.github.com>
2018-09-12 20:27:11 +0800

    这是第二次修改完善

--- java1.java
+++ java1.java
@@ -2,6 +2,6 @@ public class A{
   public static void main(String args[]) {
     String str = null;
     str = "This is my study's git!";
-    System.out.println("这是我学习的git!");
+    System.out.println("str");
   }
 }
